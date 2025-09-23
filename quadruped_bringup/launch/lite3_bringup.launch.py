b5428d2989607c2f4b4a14ac2b55f3ed798cd2e6
RedCloud79 <120488365+RedCloud79@users.noreply.github.com>
2025-09-23 14:52:37 +0900

    update urdf

---
+++ quadruped_bringup/launch/lite3_bringup.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    nav2_pkg_share = get_package_share_directory('nav2_bringup')
+    urdf_file = os.path.join(nav2_pkg_share, 'urdf', 'Lite3.urdf')
+
+    quad_pkg_share = get_package_share_directory('quadruped_bringup')
+    rviz_config = os.path.join(quad_pkg_share, 'rviz', 'quadruped.rviz')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': False}],
+            arguments=[urdf_file]
+        ),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config]
+        ),
+    ])
